2c5561a0c57ba8ce7e0cd9c82e0a780135fc0d94
Pouya <pouyamohammadyirbu@gmail.com>
2025-04-24 22:10:04 +0330

    fix(direction): rtl issue, implementing it properly and adding locale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { Provider } from '@/components/ui/provider';
 import '@/styles/globals.css';
+import { LocaleProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider>
-      <Component {...pageProps} />
+      {/* TODO: develop for en also ? */}
+      <LocaleProvider locale="fa-Ir">
+        <Component {...pageProps} />
+      </LocaleProvider>
     </Provider>
   );
 }
