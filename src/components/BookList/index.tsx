b32e3ee60a21c1ceb95dfe12043380877c4daabc
Pouya <pouyamohammadyirbu@gmail.com>
2025-04-24 21:21:01 +0330

    feat(BookList): implementing a grid based structure for rendering list of books

---
+++ src/components/BookList/index.tsx
@@ -0,0 +1,31 @@
+import { Book } from '@/types/book';
+import { SimpleGrid, Skeleton, Alert } from '@chakra-ui/react';
+import { BookCard } from '../BookCard';
+
+type BookListProps = {
+  books: Book[];
+  isLoading: boolean;
+  isError: boolean;
+};
+
+export const BookList = ({ books, isLoading, isError }: BookListProps) => {
+  if (isError) {
+    return (
+      <Alert.Root status="error">
+        <Alert.Indicator />
+        <Alert.Title>Failed to load books</Alert.Title>
+      </Alert.Root>
+    );
+  }
+
+  return (
+    // TODO: refactor and unify styles for skeleton and card
+    <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }}>
+      {isLoading
+        ? Array(8)
+            .fill(0)
+            .map((_, i) => <Skeleton margin={4} key={i} height="400px" borderRadius="md" />)
+        : books?.map((book) => <BookCard key={book.id} book={book} />)}
+    </SimpleGrid>
+  );
+};
