cbb4795d999cc1bf54605063b658498d553c0c38
Pouya <pouyamohammadyirbu@gmail.com>
2025-04-25 10:07:37 +0330

    refactor(useFetch): implementing a more optimized fetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 
 /**
  * Contains fetched data and its status
@@ -10,32 +10,59 @@ type FetchState<T> = {
 };
 
 /**
+ * Generates query params into a string to use it inside a url
+ * @param params query params
+ * @returns generated query parameter
+ */
+const createQueryString = (params: Record<string, string>): string => {
+  const searchParams = new URLSearchParams();
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (value === undefined || value === null) return;
+    searchParams.set(key, typeof value === 'object' ? JSON.stringify(value) : value.toString());
+  });
+
+  return searchParams.toString();
+};
+
+/**
  * Generalized hook for fetching data
- * @param url api address to fetch data from
+ * @param baseUrl api address to fetch data from
+ * @param params query params
  * @returns fetch state contains data and fetch status
  */
-export const useFetch = <T>(url: string): FetchState<T> => {
+export const useFetch = <T>(baseUrl: string, params: Record<string, string> = {}) => {
+  const queryString = useMemo(() => createQueryString(params), [params]);
+  const url = useMemo(() => `${baseUrl}?${queryString}`, [baseUrl, queryString]);
   const [state, setState] = useState<FetchState<T>>({
     data: null,
     loading: true,
     error: null,
   });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error(response.statusText);
+  const fetchData = useCallback(async () => {
+    try {
+      setState((prev) => ({ ...prev, loading: true }));
 
-        const data: T = await response.json();
-        setState({ data, loading: false, error: null });
-      } catch (error) {
-        setState({ data: null, loading: false, error: error as Error });
-      }
-    };
+      const response = await fetch(url);
+      if (!response.ok) throw new Error(`HTTP ${response.status}`);
 
-    fetchData();
+      const data: T = await response.json();
+      setState({ data, loading: false, error: null });
+    } catch (error) {
+      setState({ data: null, loading: false, error: error as Error });
+    }
   }, [url]);
 
-  return state;
+  // cache result
+  const cachedFetch = useMemo(() => fetchData, [fetchData]);
+
+  useEffect(() => {
+    cachedFetch();
+  }, [cachedFetch]);
+
+  return {
+    ...state,
+    refetch: cachedFetch,
+  };
 };
