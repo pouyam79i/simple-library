cf6303d3b1a7c45eac4e8762f7a68fe65993616a
Pouya <pouyamohammadyirbu@gmail.com>
2025-04-25 10:08:24 +0330

    refactor(books-data-fetching): adjusting to new fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,23 @@ import { BookList } from '@/components/BookList';
 import { useBooks } from '@/hooks/useBooks';
 import { Box } from '@chakra-ui/react';
 
+const BASE_URL = 'https://get.taaghche.com/v2/everything';
+const INITIAL_PARAMS = {
+  filters: {
+    list: [
+      { type: 21, value: 0 },
+      { type: 6, value: -150000 },
+    ],
+  },
+  order: 1,
+};
+
 export default function Home() {
-  const { data, isLoading, isError } = useBooks(
-    'https://get.taaghche.com/v2/everything?filters=%7B%22list%22:[%7B%22type%22:21,%22value%22:0%7D,%7B%22type%22:6,%22value%22:-150000%7D,%7B%22type%22:50,%22value%22:0%7D]%7D&offset=0-0-0-16&order=1',
-  );
+  const { books, isLoading, isError } = useBooks(BASE_URL, INITIAL_PARAMS);
 
   return (
     <Box>
-      <BookList books={data.bookList.books} isLoading={isLoading} isError={isError} />
+      <BookList books={books} isLoading={isLoading} isError={isError} />
     </Box>
   );
 }
